d8de8c10017a48070bc9b95a3bdc5db15a8cbe85
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-05-21 17:53:31 -0400

    commit tree 00197be9b4faade7ca8fc846cdb51e02988b2865

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+//help needed
+let person = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards(person){
+    for(let i = 0; i < person.length; i++){
+        return(`Thank you ${person[i]} for the gift!`)
+    }
+}
+ 
+
+// This function works correctly in the console.log -- won't pass test
+function countDown() {
+    for(let countDown = 10; countDown > -1; countDown--){
+        return countDown
+    }
+}
+countDown()
+
+
+
+
